b013e3e4c5ea9a861699036ee91a1ef3fdb753c4
zhouyulin <1273641404@qq.com>
2021-01-19 17:35:58 +0800

    测试

--- src/components/AutoTask.vue
+++ src/components/AutoTask.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
-    <el-table :data="tableData" style="width: 100%" height="250">
-      <el-table-column
-        fixed
-        prop="timerName"
-        label="自动任务名称"
-        width="150"
-      />
+    <el-button type="primary" @click="demo">主要按钮</el-button>
+    <el-table :data="tdata" style="width: 100%" height="250">
+      <el-table-column fixed prop="timerName" label="自动任务名称" width="150"/>
       <el-table-column prop="timerDesc" label="自动任务说明" width="120" />
       <el-table-column prop="cron" label="cron表达式" width="120" />
       <el-table-column prop="group" label="分组" width="120" />
@@ -21,28 +17,25 @@ export default {
   name: "AutoTask",
   data() {
     return {
-      tableData: [] 
-    }
+      tdata: [],
+    };
   },
-  method(){
-    this.demo();
-  },
-  methods: {
-    demo() {
-      //请求实体
+  created:function(){
+    let _this=this;
+    //请求实体
       const request = {
-        url: `http://localhost:8080/timer/task/jobDetails`,
+        url: `http://120.92.145.112:8080/timer/task/jobDetails`,
         method: "get", //指定请求方式
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
         },
       };
-      this.$api(request, (err, resp, data) => {
-        debugger;
-        console.log(data);
+      this.$api(request).then(function (response) {
+        console.log(response.data.data.items);
+        _this.tdata=response.data.data.items;
       });
-    },
   },
+
 };
 </script>
 
